b0e0a5da9a9b4c0d11fff876fb5a0a41c9e7d0ac
Sixten Tamleht <sixten.tamleht@elev.ga.ntig.se>
2025-09-10 11:13:25 +0200

    start

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Characters
+{
+	class Fighter
+	{
+		public string name;
+		public int hp;
+		public float maxDamage;
+		public float minDamage;
+		public int chanseToClit;
+		public float clittModifyer;
+		
+		public Fighter(string Name, int Hp, float MaxDamage, float MinDamage, int ChanseToClit, float ClittModifyer)
+		{
+			name=Name;
+			hp=Hp;
+			maxDamage=MaxDamage;
+			minDamage=MinDamage;
+			chanseToClit=ChanseToClit;
+			clittModifyer=ClittModifyer;
+		}
+		
+	}
+	
+}
+
+class GameFunctions
+{
+	static public int question()
+	{
+		string
+		while(
+	}
+	static public bool hasPlayedBefore()
+	{
+		return false;
+	}
+	static public void tutorial()
+	{
+
+	}
+}
+
+class FightSpel
+{
+	public static void Main()
+	{
+		int answear;
+		//Characters.Fighter fighter = new Characters.Fighter{"", 0, 0.0f, 0.0f, 0, 0.0f};
+		Console.WriteLine("WELCOME!! To the Serek MMA coaching, do you want the tutorial?\n[1]: yes, [2]:no");
+
+	}
+}
